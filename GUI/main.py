b491b0ff064a55bae7416d82975035b6f879ca39
Erick Orellana <earnelgeof@gmail.com>
2024-10-23 14:48:24 -0400

    Moved layouts into individual functions, collapsed AWG panel

--- GUI/main.py
+++ GUI/main.py
@@ -5,7 +5,7 @@ from PyQt6.QtCore import Qt
 
 #html standard colors
 colors = ["Yellow","Teal","Silver","Red","Purple","Olive","Navy","White",
-    "Maroon","Lime","Green","Gray","Fuchsia","Blue","Black","Aqua"]
+            "Maroon","Lime","Green","Gray","Fuchsia","Blue","Black","Aqua"]
 class ColorBox(QWidget):
 
     def __init__(self, color):
@@ -17,22 +17,46 @@ class ColorBox(QWidget):
         self.setPalette(palette)
 
 class MainWindow(QMainWindow):
-
+    
     def __init__(self):
         super(MainWindow, self).__init__()
 
         self.setWindowTitle("My App")
-        #might be prudent to split each layout definition into their own functions
 
         #layout for awg section
+        awgLayout = self.awgLayoutSetup()
+
+
+        #layout for window containing oscilloscope, wave, and logic analyizer
+        centerLayout = self.centerLayoutSetup()
+        
+
+        #layout for device control
+        deviceLayout = self.deviceLayoutSetup()
+
+
+        #overall layout
+        layout = QGridLayout()
+        layout.addLayout(awgLayout, 0, 0, 2, -1)#for awg
+        layout.addLayout(centerLayout, 2, 0, 7,-1) #for center block
+        layout.addLayout(deviceLayout, 9, 0,1,-1) #for connect/disconect
+
+
+        widget = QWidget()
+        widget.setLayout(layout)
+        self.setCentralWidget(widget)
+        self.resize(400,500)
+
+        
+    def awgLayoutSetup(self):
         awgLayout = QGridLayout()
         awgLayout.addWidget(ColorBox(colors[0]),0,0, -1,-1)#background for demonstration. Remove later
         #enable channel
             #to be able to use these outside of init, we'll need to prepend self as in "self.ch1En"
         ch1En = QCheckBox("Channel 1")
         ch2En = QCheckBox("Channel 2")
-        awgLayout.addWidget(ch1En,1,0)
-        awgLayout.addWidget(ch2En,2,0)
+        awgLayout.addWidget(ch1En,0,0)
+        awgLayout.addWidget(ch2En,1,0)
 
         #wave types available
         waves = ["Sine","Square","Triangle","Sawtooth","Abritrary"]
@@ -40,50 +64,49 @@ class MainWindow(QMainWindow):
         ch2WaveSelect = QComboBox() 
         ch1WaveSelect.addItems(waves)
         ch2WaveSelect.addItems(waves)
-        awgLayout.addWidget(ch1WaveSelect,1,1)
-        awgLayout.addWidget(ch2WaveSelect,2,1)
+        awgLayout.addWidget(ch1WaveSelect,0,1)
+        awgLayout.addWidget(ch2WaveSelect,1,1)
 
         #AWG frequencies
         ch1Freq = ColorBox(colors[1])
-        awgLayout.addWidget(ch1Freq,1,2)
-        awgLayout.addWidget(QLabel("Frequency (Hz):"),1,2)
+        awgLayout.addWidget(ch1Freq,0,2)
+        awgLayout.addWidget(QLabel("Frequency (Hz):"),0,2)
         ch2Freq = ColorBox(colors[2])
-        awgLayout.addWidget(ch2Freq,2,2)
-        awgLayout.addWidget(QLabel("Frequency (Hz):"),2,2)
+        awgLayout.addWidget(ch2Freq,1,2)
+        awgLayout.addWidget(QLabel("Frequency (Hz):"),1,2)
             
         #AWG amplitueds
         ch1Amp = ColorBox(colors[3])
-        awgLayout.addWidget(ch1Amp,1,3)
-        awgLayout.addWidget(QLabel("Amplitude (V):"),1,3)
+        awgLayout.addWidget(ch1Amp,0,3)
+        awgLayout.addWidget(QLabel("Amplitude (V):"),0,3)
         ch2Amp = ColorBox(colors[4])
-        awgLayout.addWidget(ch2Amp,2,3)        
-        awgLayout.addWidget(QLabel("Amplitude (V):"),2,3)
+        awgLayout.addWidget(ch2Amp,1,3)        
+        awgLayout.addWidget(QLabel("Amplitude (V):"),1,3)
 
         #AWG offsets
         ch1Off = ColorBox(colors[5])
-        awgLayout.addWidget(ch1Off,1,4)
-        awgLayout.addWidget(QLabel("Offset (V):"),1,4)
+        awgLayout.addWidget(ch1Off,0,4)
+        awgLayout.addWidget(QLabel("Offset (V):"),0,4)
         ch2Off = ColorBox(colors[7])#6 doesn't play nice with black text
-        awgLayout.addWidget(ch2Off,2,4)        
-        awgLayout.addWidget(QLabel("Offset (V):"),2,4)
+        awgLayout.addWidget(ch2Off,1,4)        
+        awgLayout.addWidget(QLabel("Offset (V):"),1,4)
 
         awgPhase = ColorBox(colors[8])
-        awgLayout.addWidget(awgPhase,1,5)
-        awgLayout.addWidget(QLabel("Phase (°):"),1,5)
-        awgLayout.addWidget(QLabel("Sync Status:"),2,5)
+        awgLayout.addWidget(awgPhase,0,5)
+        awgLayout.addWidget(QLabel("Phase (°):"),0,5)
+        awgLayout.addWidget(QLabel("Sync Status:"),1,5)
 
         syncButton = QPushButton("FORCE SYNC")
         buttonPalette = syncButton.palette()
         buttonPalette.setColor(QPalette.ColorRole.ButtonText, QColor("red"))
         syncButton.setPalette(buttonPalette)
-        awgLayout.addWidget(syncButton,2,6)
+        awgLayout.addWidget(syncButton,1,6)
         awgLayout.addWidget(QLabel("Waveform Generator"),0,6)
 
+        return awgLayout
+    
 
-
-
-
-        #layout for window containing oscilloscope, wave, and logic analyizer
+    def centerLayoutSetup(self):
         centerLayout = QGridLayout()
         centerLayout.addWidget(ColorBox(colors[1]),0,0, -1,-1)#background for demonstration. Remove later
         runStopButton = QPushButton("RUN/STOP")
@@ -113,7 +136,7 @@ class MainWindow(QMainWindow):
 
         dataLayout = QGridLayout()
         dataLayout.addWidget(ColorBox("lime"),0,0,-1,-1)#background for demonstration. Remove later
-        dataLayout.addWidget(QLabel("Live Data",alignment = Qt.AlignmentFlag.AlignTop),0,0,-1,-1)
+        dataLayout.addWidget(QLabel("Live Data",alignment = (Qt.AlignmentFlag.AlignTop | Qt.AlignmentFlag.AlignHCenter)),0,0,-1,-1)
         
 
         oscilloLayout = QGridLayout()
@@ -132,15 +155,16 @@ class MainWindow(QMainWindow):
         centerLayout.addWidget(singleButton,0,1)
         centerLayout.addWidget(dataButton,0,2)
         centerLayout.addWidget(QLabel("Live Data"),0,9)
-        
 
-        #layout for device control
+        return centerLayout
+
+    def deviceLayoutSetup(self):
         deviceLayout = QGridLayout()
         deviceLayout.addWidget(ColorBox(colors[2]),0,0, -1,-1)#background for demonstration. Remove later
         deviceLayout.addWidget(QLabel("Device Status"), 0,0,1,1)
         #specific colored buttons
         connectButton = QPushButton("CONNECT")
-        #buttonPalette = connectButton.palette()
+        buttonPalette = connectButton.palette()
         
         buttonPalette.setColor(QPalette.ColorRole.Button, QColor("green"))
         buttonPalette.setColor(QPalette.ColorRole.ButtonText, QColor("white"))
@@ -161,19 +185,8 @@ class MainWindow(QMainWindow):
         deviceLayout.setColumnStretch(1,1) 
         deviceLayout.setColumnStretch(0,1) 
 
+        return deviceLayout
 
-
-        #overall layout
-        layout = QGridLayout()
-        layout.addLayout(awgLayout, 0, 0, 2, -1)#for awg
-        layout.addLayout(centerLayout, 2, 0, 7,-1) #for center block
-        layout.addLayout(deviceLayout, 9, 0,1,-1) #for connect/disconect
-
-
-        widget = QWidget()
-        widget.setLayout(layout)
-        self.setCentralWidget(widget)
-        self.resize(400,500)
 
 app = QApplication(sys.argv)
 
