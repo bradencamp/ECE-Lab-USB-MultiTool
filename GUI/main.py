4398efb7e83942e9b331b959533b55b702db997f
amai8gmu <amai8@gmu.edu>
2024-11-12 20:40:40 -0500

    minor changes & added comments. still trying to get scope channel checkboxes working

--- GUI/main.py
+++ GUI/main.py
@@ -21,9 +21,11 @@ prefixes_voltage = {"m": 1e-3,"":1}
 prefixes_frequency = {"k": 1e3, "M": 1e6,"":1}
 
 #arbitrary number used in update_plot to check which checkbox is checked, initialize to 0
-checked = 0
+awgChecked = 0
 
-runStopClicked = 0
+runStopClicked = 0#holds state of runStopButton
+oscChecked = 0#holds state of scope checkbox
+oscCHChecked = 0#holds state of scope channel checkboxes
 
 class ColorBox(QWidget):
 
@@ -88,9 +90,9 @@ class MainWindow(QMainWindow):
         self.ch2En.setChecked(True)
         awgLayout.addWidget(self.ch1En,0,0)
         awgLayout.addWidget(self.ch2En,1,0)
-        #connect stateChanged signals for ch1 & ch2 from QCheckBox to onStateChanged method/slot
-        self.ch1En.stateChanged.connect(self.onStateChanged)
-        self.ch2En.stateChanged.connect(self.onStateChanged)
+        #connect stateChanged signals for awg ch1 & ch2 from QCheckBox to awgChStateChanged method/slot
+        self.ch1En.stateChanged.connect(self.awgChStateChanged)
+        self.ch2En.stateChanged.connect(self.awgChStateChanged)
 
         #wave types available
         waves = ["Sine","Square","Triangle","Sawtooth","Abritrary"]
@@ -154,7 +156,10 @@ class MainWindow(QMainWindow):
         centerSettings.addWidget(QLabel("Trigger Settings: "),0,1,1,1)
 
         trigTypeSelect = QComboBox()
-        trigTypeSelect.addItem("Trigger Types")
+        mode = ["Auto","Normal", "None"]
+        #trigTypeSelect.addItem("Trigger Types")
+        trigTypeSelect.setPlaceholderText('Trigger Mode')
+        trigTypeSelect.addItems(mode)
         trigHoldoffSelect = QComboBox()
         trigHoldoffSelect.addItem("Holdoff (s)")
 
@@ -175,6 +180,7 @@ class MainWindow(QMainWindow):
             pos = i+1
             self.logic_checks[i] = QCheckBox(f"Channel {pos}")
             self.logic_edges[i] = QComboBox()
+            self.logic_edges[i].setPlaceholderText('Trigger Type')
             self.logic_edges[i].addItems(edges)
             logicLayout.addWidget(self.logic_checks[i],i+1,0)
             logicLayout.addWidget(self.logic_edges[i],i+1,1)
@@ -199,6 +205,7 @@ class MainWindow(QMainWindow):
         self.zeros = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]#array of 0's for when no channel checkbox selected
         self.plot_graph.setLabel("left", "Voltage (V)")
         self.plot_graph.setLabel("bottom", "Time (s)",)
+        self.plot_graph.setYRange(-5, 5)
         self.ch1Line = self.plot_graph.plot(self.time, self.temperature,pen = self.pen1)
         self.ch2Line = self.plot_graph.plot(self.time, self.temperature,pen = self.pen2)
 
@@ -207,17 +214,28 @@ class MainWindow(QMainWindow):
 
         oscilloLayout = QGridLayout()
         oscilloLayout.addWidget(ColorBox("Olive"),0,0,-1,-1)#background for demonstration. Remove later
-        oscilloCheck = QCheckBox()#QLabel("Logic Analyzer",alignment = Qt.AlignmentFlag.AlignTop)#QCheckBox("Logic Analyzer")
-        oscilloLayout.addWidget(oscilloCheck,0,0)
+        self.oscilloCheck = QCheckBox()#QLabel("Logic Analyzer",alignment = Qt.AlignmentFlag.AlignTop)#QCheckBox("Logic Analyzer")
+        self.oscilloCheck.setChecked(True)#default scope checkbox checked -> scope on
+        self.oscilloCheck.stateChanged.connect(self.oscStateChanged)#method/slot for checking scope checkbox state
+        oscilloLayout.addWidget(self.oscilloCheck,0,0)
         oscilloLayout.addWidget(QLabel("Oscilloscope"),0,1,alignment = Qt.AlignmentFlag.AlignHCenter)
         
         self.oscCh1EN = QCheckBox("CH1")
+        #initially set scope ch1 checkbox to selected/on
+        self.oscCh1EN.setChecked(True)
+        #connect stateChanged signal for scope ch1 from QCheckBox to onStateChanged method/slot
+        self.oscCh1EN.stateChanged.connect(self.oscChStateChanged)
         self.oscCh1Trig = QComboBox()
+        self.oscCh1Trig.setPlaceholderText('Trigger Type')
         self.oscCh1Trig.addItems(edges)
         self.oscCh1VDiv = LabelField("Range",[1e-6,20],1.0,2,"V/Div",{"u":1e-6,"m":1e-3,"":1},BLANK)
 
         self.oscCh2EN = QCheckBox("CH2")
         self.oscCh2Trig = QComboBox()
+        self.oscCh2EN.setChecked(True)#initially set scope ch2 checkbox to selected/on
+        #connect stateChanged signal for scope ch2 from QCheckBox to onStateChanged method/slot
+        self.oscCh2EN.stateChanged.connect(self.oscChStateChanged)
+        self.oscCh2Trig.setPlaceholderText('Trigger Type')
         self.oscCh2Trig.addItems(edges)
         self.oscCh2VDiv = LabelField("Range",[1e-6,20],1.0,2,"V/Div",{"u":1e-6,"m":1e-3,"":1},BLANK)
         
@@ -272,55 +290,94 @@ class MainWindow(QMainWindow):
 
         return deviceLayout
 
-    def button_was_clicked(self):
-        if self.runStopButton.isChecked():
+    def button_was_clicked(self):#checks if runStopButton was clicked/unclicked
+        if self.runStopButton.isChecked():#runStopButton clicked to run
+            self.runStopButton.setChecked(True)#set button clicked
             runStopClicked = 1
-            self.runStopButton.setChecked(True)
-        else:
+        else:#runStopButton unclicked to stop
+            self.runStopButton.setChecked(False)#set button unclicked
             runStopClicked = 0
-            self.runStopButton.setChecked(False)
         return runStopClicked
 
-    def onStateChanged(self):#tracks state of ch1 & ch2 checkboxes
-        #only ch1 checkbox selected
-        if self.ch1En.isChecked() and not self.ch2En.isChecked():
-            checked = 1
-        #only ch2 checkbox selected
-        elif not self.ch1En.isChecked() and self.ch2En.isChecked():
-            checked = 2
-        #both ch1 & ch2 checkbox selected
-        elif self.ch1En.isChecked() and self.ch2En.isChecked():
-            checked = 3
-        #no checkbox selected
+    def oscStateChanged(self):#tracks state of oscilloscope checkbox 
+        if self.oscilloCheck.isChecked():
+            oscChecked = 1#scope on
         else:
-            checked = 0
+            oscChecked = 0#scope off
+        return oscChecked#return scope checkbox state
 
-        return checked#return which checkbox is checked
+    def awgChStateChanged(self):#tracks state of awg ch1 & ch2 checkboxes
+        #only ch1 awg checkbox selected
+        if self.ch1En.isChecked() and not self.ch2En.isChecked():
+            awgChecked = 1
+        #only ch2 awg checkbox selected
+        elif not self.ch1En.isChecked() and self.ch2En.isChecked():
+            awgChecked = 2
+        #both ch1 & ch2 awg checkbox selected
+        elif self.ch1En.isChecked() and self.ch2En.isChecked():
+            awgChecked = 3
+        #no awg checkbox selected
+        else:
+            awgChecked = 0
+        return awgChecked#return which checkbox is checked
+    
+    def oscChStateChanged(self):#tracks state of osc ch1 & ch2 checkboxes
+        #only ch1 scope checkbox selected
+        if self.oscCh1EN.isChecked() and not self.oscCh2EN.isChecked():
+            self.oscCh1EN.setChecked(True)
+            self.oscCh2EN.setChecked(False)
+            oscCHChecked = 1
+        #only ch2 scope checkbox selected
+        elif not self.oscCh1EN.isChecked() and self.oscCh2EN.isChecked():
+            self.oscCh1EN.setChecked(False)
+            self.oscCh2EN.setChecked(True)
+            oscCHChecked = 2
+        #both ch1 & ch2 scope checkbox selected
+        elif self.oscCh1EN.isChecked() and self.oscCh2EN.isChecked():
+            self.oscCh1EN.setChecked(True)
+            self.oscCh2EN.setChecked(True)
+            oscCHChecked = 3
+        #no scope checkbox selected
+        else:
+            self.oscCh1EN.setChecked(False)
+            self.oscCh2EN.setChecked(False)
+            oscCHChecked = 0
+        return oscCHChecked#return which scope checkbox is checked
    
+    def plotCh1(self):#plot ch1 data
+        self.ch1Line.setData(self.time, self.temperature)#plot ch1 data
+        self.ch2Line.setData(self.zeros, self.zeros)#plot zeros for ch2, works for now
+
+    def plotCh2(self):#plot ch2 data
+        self.ch1Line.setData(self.zeros, self.zeros)
+        self.ch2Line.setData(self.time, self.temperature)
+
+    def plotCh12(self):#plot ch1 & ch2 data
+        self.ch1Line.setData(self.time, self.temperature) 
+        self.ch2Line.setData(self.time, self.temperature)
+    
+    def plotZero(self):#no plotting
+        self.ch1Line.setData(self.zeros, self.zeros)
+        self.ch2Line.setData(self.zeros, self.zeros)
+
     def update_plot(self):
         self.time = self.time[1:]
         self.time.append(self.time[-1] + 1)
         self.temperature = self.temperature[1:]
         self.temperature.append(uniform(-5, 5))   
-        #checked=1
-        if self.onStateChanged() == 1 and self.button_was_clicked():
-            self.ch1Line.setData(self.time, self.temperature)#plot ch1 data
-            self.ch2Line.setData(self.zeros, self.zeros)#plot zeros for ch2, works for now
-        #checked=2
-        elif self.onStateChanged() == 2 and self.button_was_clicked():
-            self.ch2Line.setData(self.time, self.temperature)
-            self.ch1Line.setData(self.zeros, self.zeros)
-        #checked=3
-        elif self.onStateChanged() == 3 and self.button_was_clicked():
-            self.ch1Line.setData(self.time, self.temperature)
-            self.ch2Line.setData(self.time, self.temperature)
-        #checked=0
-        else:
-            self.ch1Line.setData(self.zeros, self.zeros)
-            self.ch2Line.setData(self.zeros, self.zeros)
+        #awgChecked=1 run/stop button clicked, oscChecked=1, 
+        if self.awgChStateChanged() == 1 and self.button_was_clicked() and self.oscStateChanged():
+            self.plotCh1()#plot only ch1
+        #awgChecked=2 run/stop button clicked, oscChecked=1, 
+        elif self.awgChStateChanged() == 2 and self.button_was_clicked() and self.oscStateChanged():
+            self.plotCh2()#plot only ch2
+        #awgChecked=3 run/stop button clicked, oscChecked=1, 
+        elif self.awgChStateChanged() == 3 and self.button_was_clicked() and self.oscStateChanged():
+            self.plotCh12()#plot both ch1 & ch2
+        #awgChecked=0 run/stop button clicked, oscChecked=1, 
+        else:#don't plot anything
+            self.plotZero()
 
-            
-            
 app = QApplication(sys.argv)
 
 window = MainWindow()
